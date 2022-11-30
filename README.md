17034fedeb2fef3a08253a52d413488f0ba46997
lmoraesdev <lbc_moraes@outlook.com>
2022-11-30 01:01:32 -0300

    feat: atualizarLivro

---
+++ README.md
@@ -0,0 +1,3 @@
+Estudo de api REST com node.js
+ node.js com express.
+ usando banco de dandos mongodb.
--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,11 +1,33 @@
-import livros from "../models/Livro.js"
+import livros from "../models/Livro.js";
 
 class livroController {
+
 	static listarLivros = (req, res) => {
-		livros.find((err, livros) =>{
-			res.status(200).json(livros)
-		})
-	}
+		livros.find((err, livros) => {
+			res.status(200).json(livros);
+		});
+	};
+
+	static cadastrarLivro = (req, res) => {
+		let livro = new livros(req.body);
+		livro.save((err) => {
+			if (err) {
+				res
+					.status(500)
+					.send({ message: `${err.message} - falha ao cadastrar livro.` });
+			} else {
+				res.status(201).send(livro.toJSON());
+			}
+		});
+	};
+
+	static atualizarLivro = (req, res) => {
+		const id = req.params.id;
+
+		livros.findByIdAndUpdate(id, {$set: "req.body"})
 }
 
-export default LivroController
+}
+
+
+export default LivroController;
